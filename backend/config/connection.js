54c2424102607f1e8b5934491f6e7939a823768e
lindsey5 <lindseysamson5@gmail.com>
2025-11-05 17:47:03 +0800

    initial commit

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -2,17 +2,22 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  },
-  port: 4058,
-  logging: false
-});
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
+    logging: false,
+    dialectOptions: process.env.DB_AUTHORIZED === 'true' ? {
+      ssl: {
+        rejectUnauthorized: process.env.DB_AUTHORIZED === 'true'
+      }
+    } : {}
+  }
+);
 
 // Function to test database connection
 const connectDB = async () => {
