ca922870525b4221a2f6a11e43ee2b2fa0087379
lindsey5 <lindseysamson5@gmail.com>
2025-11-10 21:38:38 +0800

    initial commit

--- frontend/src/contexts/Socket.jsx
+++ frontend/src/contexts/Socket.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import { createContext } from 'react';
 
-const SOCKET_URL = process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000';
+const SOCKET_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000';
 
 export const SocketContext = createContext();
 
